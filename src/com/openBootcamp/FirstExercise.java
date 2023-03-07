ec9055c3dafb4b243782f6c5d8de5d141f196074
ManuHG1805 <zherrera1995@gmail.com>
2023-03-07 11:45:56 +0100

    First commit

---
+++ src/com/openBootcamp/FirstExercise.java
@@ -0,0 +1,40 @@
+package com.openBootcamp;
+
+public class FirstExercise {
+
+    public static void main(String[] args) {
+        // 1.Numbers
+
+        // 1.2.Integers
+        byte typeByte = 1;
+        System.out.println("byte:"+typeByte);
+        short typeShort = 10;
+        System.out.println("short:"+typeShort);
+        int typeInt = 100;
+        System.out.println("int:"+typeInt);
+        long typeLong = 1000;
+        System.out.println("long:"+typeLong);
+
+        // 1.3.Floating point
+        float typeFloat = 1.5f;
+        System.out.println("float:"+typeFloat);
+        double typeDouble = 10.05d;
+        System.out.println("double:"+typeDouble);
+
+
+
+        // 2.Boolean
+        boolean typeTrue = true;
+        System.out.println("boolean:"+typeTrue);
+        boolean typeFalse = false;
+        System.out.println("boolean:"+typeFalse);
+
+        // 3.Text
+        char typeChar ='e';
+        System.out.println("char:"+typeChar);
+        String typeString = "Hello World";
+        System.out.println("String:"+typeString);
+
+
+    }
+}
