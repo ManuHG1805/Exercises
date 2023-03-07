91b3bf9aa484855c9d72ebdbc66d5d6cc54b4c78
ManuHG1805 <zherrera1995@gmail.com>
2023-03-07 12:55:52 +0100

    Documentation commit

--- src/com/openBootcamp/FirstExercise.java
+++ src/com/openBootcamp/FirstExercise.java
@@ -1,5 +1,11 @@
 package com.openBootcamp;
 
+/**
+ * Para este primer ejercicio tendréis que realizar lo siguiente:
+ *  -Crea un proyecto de Java desde 0
+ *  -Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+ *  -Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+ */
 public class FirstExercise {
 
     public static void main(String[] args) {
